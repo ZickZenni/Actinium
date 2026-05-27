6b3d3f19640bf1740959fac5427bac46b1bda0c3
ZickZenni <zickzenniyt@gmail.com>
2026-05-27 16:02:44 +0200

    Fix various warnings inside `instance_view.h`

--- src/ui/view/instance_view.h
+++ src/ui/view/instance_view.h
@@ -11,11 +11,11 @@ namespace Actinium
     public:
         explicit InstanceView(QWidget *parent = nullptr);
 
-        void scrollTo(const QModelIndex &index, ScrollHint hint = EnsureVisible) override;
+        void scrollTo(const QModelIndex &index, ScrollHint hint) override;
 
-        QModelIndex indexAt(const QPoint &point) const override;
+        [[nodiscard]] QModelIndex indexAt(const QPoint &point) const override;
 
-        QRect visualRect(const QModelIndex &index) const override;
+        [[nodiscard]] QRect visualRect(const QModelIndex &index) const override;
 
     signals:
         void SelectionChanged(const QModelIndex &index);
@@ -29,17 +29,17 @@ namespace Actinium
 
         void mouseMoveEvent(QMouseEvent *event) override;
 
-        int verticalOffset() const override;
+        [[nodiscard]] int verticalOffset() const override;
 
-        int horizontalOffset() const override;
+        [[nodiscard]] int horizontalOffset() const override;
 
-        bool isIndexHidden(const QModelIndex &index) const override;
+        [[nodiscard]] bool isIndexHidden(const QModelIndex &index) const override;
 
         QModelIndex moveCursor(CursorAction action, Qt::KeyboardModifiers modifiers) override;
 
         void setSelection(const QRect &rect, QItemSelectionModel::SelectionFlags command) override;
 
-        QRegion visualRegionForSelection(const QItemSelection &selection) const override;
+        [[nodiscard]] QRegion visualRegionForSelection(const QItemSelection &selection) const override;
 
     private:
         int m_items_per_row;
@@ -49,8 +49,8 @@ namespace Actinium
 
         void PaintWelcomeMessage(QPainter& painter) const;
 
-        int GetContentWidth() const;
+        [[nodiscard]] int GetContentWidth() const;
 
-        int CalculateItemsPerRow() const;
+        [[nodiscard]] int CalculateItemsPerRow() const;
     };
 }
